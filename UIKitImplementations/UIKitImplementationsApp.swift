5261b20e5303193027769e8c9669d0f36ceacff4
Fran O <contact@fran-ochoa.es>
2024-07-26 17:18:46 +0200

    Initial Commit

---
+++ UIKitImplementations/UIKitImplementationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitImplementationsApp.swift
+//  UIKitImplementations
+//
+//  Created by Fran Ochoa on 26/7/24.
+//
+
+import SwiftUI
+
+@main
+struct UIKitImplementationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
